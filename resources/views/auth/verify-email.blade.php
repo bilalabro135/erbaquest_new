08f99c05bc694573beb9c6e06564e1a5a2ba5a74
balouchgeeks <balouch@geeksroot.com>
2022-03-21 13:12:44 +0500

    verify email template

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.front.app', ['body_class' => 'bg-gradient-primary', 'title' => 'Verify Email', 'sidebar' => false, 'topbar' => false])
 
 @section('content')
+
+<section class="inner-banner" style="background: url({{env('APP_URL').'storage/photos/1/inner-banner.jpg'}});">
+  <div class="container">
+    <h1 class="vc_heading ft-saira text-center">Verification</h1>
+  </div>
+</section>
     <div class="container">
         <div class="row justify-content-center align-items-center"  style="height: 100vh;">
             <div class="col-md-8">
