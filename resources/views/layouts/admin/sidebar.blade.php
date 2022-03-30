204c7c9e755fd3ae578de12c4f775ef774e0da6e
balouchgeeks <balouch@geeksroot.com>
2022-03-30 05:30:59 +0500

    admin sidebar

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -1,10 +1,11 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('admin') }}">
-                <div class="sidebar-brand-icon rotate-n-15">
+                <img src="{{asset('images/admin_logo.png')}}">
+                <!-- <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">{{ ($globalsettings->getValue( 'site_name')) ? $globalsettings->getValue('site_name') : config('app.name', 'Laravel') }}</div>
+                <div class="sidebar-brand-text mx-3">{{ ($globalsettings->getValue( 'site_name')) ? $globalsettings->getValue('site_name') : config('app.name', 'Laravel') }}</div> -->
             </a>
 
             <hr class="sidebar-divider my-0">
