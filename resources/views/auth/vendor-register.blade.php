32dbdf3c393a3c8bf68a3fa25ad8bc6d82ca8ccb
areebg <areeb.ghouri@geeksroot.com>
2022-06-01 06:36:03 +0500

    live authoriz net

--- resources/views/auth/vendor-register.blade.php
+++ resources/views/auth/vendor-register.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
     <script type="text/javascript"
-        src="https://jstest.authorize.net/v1/Accept.js"
+        src="https://js.authorize.net/v1/Accept.js"
         charset="utf-8">
     </script>
 
@@ -338,8 +338,8 @@ $(document).on('click', 'a[href^="#"]', function(e) {
 function sendPaymentDataToAnet(argument) {
     $("p.error").hide();
     var authData = {};
-        authData.clientKey = "5553dBs9wtXUU3vdmC9aL8NBhSXu2swQ5gTqKcf7h8g75h6B9j23XbSmt2q26ECR";
-        authData.apiLoginID = "2YFQ87mYem";
+        authData.clientKey = "2nE869m3mC3PpfF7PR2V8Hwbw6Jk63nR3sem4EcSQ7Ye3Cp2GAZ9bYzmr6zDQaQn";
+        authData.apiLoginID = "4B59B5F5kv4";
 
         var insertCreditCard = document.getElementById("cardNumber").value;
 
