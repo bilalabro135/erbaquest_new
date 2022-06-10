25d7ba0b62e19ea37f7120aae2e4f2eb48dfc435
areebg <areeb.ghouri@geeksroot.com>
2022-06-10 07:02:36 +0500

    quick test

--- app/Http/Controllers/Front/SubscriptionController.php
+++ app/Http/Controllers/Front/SubscriptionController.php
@@ -279,7 +279,7 @@ class SubscriptionController extends Controller
                     echo 'not working';
                 }
 
-                echo '<pre>'; print_r( $tresponse->getMessages() ); echo '</pre>';
+                echo '<pre>'; print_r( $tresponse ); echo '</pre>';
 
                 exit;
             
